daa00031fe3447dd2c34dc494d4808c6459b7f33
0yvz <oyvindg@live.no>
2023-09-12 00:43:17 +0200

    Added about page

---
+++ components/omoss.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function OmOss() {
+  return (
+    <section className="">
+      <h2 className="text-3xl font-medium mb-6">Om Oss</h2>
+      <p></p>
+    </section>
+  );
+}
