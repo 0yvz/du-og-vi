945cf4152f410926f0069cc19b221a88d9007106
0yvz <oyvindg@live.no>
2023-09-12 00:42:26 +0200

    Added section divider

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-import Image from "next/image";
+"use client";
+
+import Hjem from "@/components/hjem";
+import OmOss from "@/components/omoss";
+import SectionDivider from "@/components/section-divider";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between ml-10 mr-10 sm:ml-14 sm:mr-14">
+      <Hjem />
+      <SectionDivider />
+      <OmOss />
+    </main>
   );
 }
---
+++ components/section-divider.tsx
@@ -0,0 +1,10 @@
+"Use client";
+
+import React from "react";
+import { motion } from "framer-motion";
+
+export default function SectionDivider() {
+  return (
+    <motion.div className="bg-gray-200 my-36 h-16 w-1 rounded-full hidden sm:block"></motion.div>
+  );
+}
