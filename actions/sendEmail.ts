556704297ed9cb2f9f513dbf6ff14447fc42e502
0yvz <oyvindg@live.no>
2023-09-14 21:32:01 +0200

    Added string validation for error

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -4,13 +4,26 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+const validateString = (value: unknown) => {
+	if (!value || typeof value !== "string") {
+		return false;
+	}
+
+	return true;
+}
+
 export const sendEmail = async (formData: FormData) => {
 	const senderEmail = formData.get("email");
 	const message = formData.get("message");
 
-	if (!message || typeof message !== "string") {
+	if (!validateString(senderEmail)){
 		return {
-			error: "Missing email or message",
+			error: "Invalid sender email"
+		}
+	}
+	if (!validateString(message)){
+		return {
+			error: "Invalid message"
 		}
 	}
 
