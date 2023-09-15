176367882a26b76f2f34e1410b6c91664e1b6199
0yvz <oyvindg@live.no>
2023-09-15 22:29:31 +0200

    Moved string validation

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,27 +1,20 @@
 "use server";
 
 import { Resend } from "resend";
+import { validateString } from "@/lib/utils";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-
-const validateString = (value: unknown) => {
-	if (!value || typeof value !== "string") {
-		return false;
-	}
-
-	return true;
-}
 
 export const sendEmail = async (formData: FormData) => {
 	const senderEmail = formData.get("email");
 	const message = formData.get("message");
 
-	if (!validateString(senderEmail)){
+	if (!validateString(senderEmail, 500)){
 		return {
 			error: "Invalid sender email"
 		}
 	}
-	if (!validateString(message)){
+	if (!validateString(message, 5000)){
 		return {
 			error: "Invalid message"
 		}
