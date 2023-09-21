6a4e0b3bbc3fcba674632515828aa81ac60aa7eb
0yvz <oyvindg@live.no>
2023-09-21 16:42:30 +0200

    Added name fields to form

--- email/contact-form-email.tsx
+++ email/contact-form-email.tsx
@@ -15,26 +15,33 @@ import { Tailwind } from "@react-email/tailwind";
 type ContactFormEmailProps = {
   message: string;
   senderEmail: string;
+  firstName: string;
+  lastName: string;
 };
 
 export default function ContactFormEmail({
   message,
   senderEmail,
+  firstName,
+  lastName,
 }: ContactFormEmailProps) {
   return (
     <Html>
       <Head />
-      <Preview>Ny melding fra duogvi.no</Preview>
+      <Preview>Ny melding fra {firstName}</Preview>
       <Tailwind>
         <Body className="bg-gray-100 text-black">
           <Container>
             <Section className="bg-white borderBlack my-10 px-10 py-4 rounded-md">
               <Heading className="leading-tight">
-                DU har fått en ny beskjed fra kontaktskjemafeltet på duogvi.no
+                DU har fått en ny beskjed fra {firstName} {lastName} i
+                kontaktskjemafeltet på duogvi.no
               </Heading>
-              <Text>{message}</Text>
+              <Text>Melding: {message}</Text>
               <Hr />
-              <Text>Senderens mail er: {senderEmail}</Text>
+              <Text>
+                Du kan kontakte {firstName} på {senderEmail}
+              </Text>
             </Section>
           </Container>
         </Body>
