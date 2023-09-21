c08d37fed9b286f5e7eb9d710ae7f882021eac48
0yvz <oyvindg@live.no>
2023-09-21 18:01:12 +0200

    Updated server validation

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -10,27 +10,41 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
   const message = formData.get("message");
+  const firstName = formData.get("firstName");
+  const lastName = formData.get("lastName");
 
   // simple server-side validation
+  if (!validateString(firstName, 30)) {
+    return {
+      error: "Feil navn",
+    };
+  }
+  if (!validateString(lastName, 30)) {
+    return {
+      error: "Feil etternavn",
+    };
+  }
   if (!validateString(senderEmail, 500)) {
     return {
-      error: "Invalid sender email",
+      error: "Feil med epost fra sender",
     };
   }
   if (!validateString(message, 5000)) {
     return {
-      error: "Invalid message",
+      error: "Feil med melding",
     };
   }
 
   let data;
   try {
     data = await resend.emails.send({
-      from: "Contact Form <onboarding@resend.dev>",
-      to: "bytegrad@gmail.com",
-      subject: "Message from contact form",
+      from: "onboarding@resend.dev",
+      to: "oyvindg@live.no",
+      subject: "Ny melding på Du&Vi",
       reply_to: senderEmail,
       react: React.createElement(ContactFormEmail, {
+        firstName: firstName,
+        lastName: lastName,
         message: message,
         senderEmail: senderEmail,
       }),
