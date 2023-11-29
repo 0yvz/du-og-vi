40613d4c6b2f4af5806f81c1ec84745827133ef2
0yvz <oyvindg@live.no>
2023-11-29 12:20:15 +0100

    Add Priser component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,21 @@ import Hjem from "@/components/hjem";
 import OmOss from "@/components/omoss";
 import SectionDivider from "@/components/section-divider";
 import Tjenester from "@/components/tjenester";
+import Priser from "@/components/priser";
 import Kontakt from "@/components/kontakt";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between ml-10 mr-10 sm:ml-14 sm:mr-14">
-      <Hjem />
-      <SectionDivider />
-      <OmOss />
-      <SectionDivider />
-      <Tjenester />
-      <SectionDivider />
-      <Kontakt />
-    </main>
-  );
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between ml-10 mr-10 sm:ml-14 sm:mr-14">
+			<Hjem />
+			<SectionDivider />
+			<OmOss />
+			<SectionDivider />
+			<Tjenester />
+			<SectionDivider />
+			<Priser />
+			<SectionDivider />
+			<Kontakt />
+		</main>
+	);
 }
