26091d863a2edecefe6e81e78740bbeea40012f1
0yvz <oyvindg@live.no>
2023-11-29 12:20:31 +0100

    Refactor sendEmail function for better readability and error handling

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -8,54 +8,54 @@ import ContactFormEmail from "@/email/contact-form-email";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (formData: FormData) => {
-  const senderEmail = formData.get("senderEmail");
-  const message = formData.get("message");
-  const firstName = formData.get("firstName");
-  const lastName = formData.get("lastName");
+	const senderEmail = formData.get("senderEmail");
+	const message = formData.get("message");
+	const firstName = formData.get("firstName");
+	const lastName = formData.get("lastName");
 
-  // simple server-side validation
-  if (!validateString(firstName, 30)) {
-    return {
-      error: "Feil navn",
-    };
-  }
-  if (!validateString(lastName, 30)) {
-    return {
-      error: "Feil etternavn",
-    };
-  }
-  if (!validateString(senderEmail, 500)) {
-    return {
-      error: "Feil med epost fra sender",
-    };
-  }
-  if (!validateString(message, 5000)) {
-    return {
-      error: "Feil med melding",
-    };
-  }
+	// simple server-side validation
+	if (!validateString(firstName, 30)) {
+		return {
+			error: "Feil navn",
+		};
+	}
+	if (!validateString(lastName, 30)) {
+		return {
+			error: "Feil etternavn",
+		};
+	}
+	if (!validateString(senderEmail, 500)) {
+		return {
+			error: "Feil med epost fra sender",
+		};
+	}
+	if (!validateString(message, 5000)) {
+		return {
+			error: "Feil med melding",
+		};
+	}
 
-  let data;
-  try {
-    data = await resend.emails.send({
-      from: "onboarding@resend.dev",
-      to: "oyvindg@live.no",
-      subject: "Ny melding på Du&Vi",
-      reply_to: senderEmail,
-      react: React.createElement(ContactFormEmail, {
-        firstName: firstName,
-        lastName: lastName,
-        message: message,
-        senderEmail: senderEmail,
-      }),
-    });
-  } catch (error: unknown) {
-    return {
-      error: getErrorMessage(error),
-    };
-  }
+	let data;
+	try {
+		data = await resend.emails.send({
+			from: "onboarding@resend.dev",
+			to: "oyvindg@live.no",
+			subject: "Ny melding på Du&Vi",
+			reply_to: senderEmail,
+			react: React.createElement(ContactFormEmail, {
+				firstName: firstName,
+				lastName: lastName,
+				message: message,
+				senderEmail: senderEmail,
+			}),
+		});
+	} catch (error: unknown) {
+		return {
+			error: getErrorMessage(error),
+		};
+	}
 
-  return {
-    data,
-  };
+	return {
+		data,
+	};
 };
