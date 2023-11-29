d5bcbfccab5a06c9be5f64cdf83593b6801989bd
0yvz <oyvindg@live.no>
2023-11-29 14:15:29 +0100

    Update prices and styling in Priser component

--- components/priser.tsx
+++ components/priser.tsx
@@ -18,7 +18,7 @@ export const PrisData = [
 		price: "750 kr",
 	},
 	{
-		title: "BPA / Følgertjenester",
+		title: "Enkle håndtverksoppgaver",
 		icon: FaHammer,
 		description: "Pris per time",
 		price: "375 kr",
@@ -35,29 +35,32 @@ export default function Priser() {
 	const { ref } = useSectionInView("Priser");
 	return (
 		<section ref={ref} id="priser" className="scroll-m-28">
-			<SectionHeading>Priser</SectionHeading>
-			<div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0">
+			<SectionHeading>
+				Priser
+				<h3 className=" text-sm">Priser ekskl. mva</h3>
+			</SectionHeading>
+			<div className="grid grid-cols-1 gap-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-8">
 				{PrisData.map((prisdata) => (
 					<div
 						key={prisdata.title}
-						className="text-center border rounded-md p-3 md:flex md:items-start md:text-left lg:block lg:text-center"
+						className="text-center bg-gray-100 border shadow-sm border-black/5 rounded-md p-4 md:flex md:items-start md:text-left lg:block lg:text-center"
 					>
 						<div className="md:flex-shrink-0 flex justify-center">
-							<div className="h-28 w-28 flex items-center justify-center">
+							<div className="h-24 w-24 flex items-center justify-center">
 								{React.createElement(prisdata.icon, {
-									className: "w-1/3 h-1/3",
+									className: "w-2/5 h-2/5 text-gray-800",
 								})}
 							</div>
 						</div>
 
-						<div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
-							<h3 className="mt-3 text-xl text-muted-foreground">
+						<div className="mt-4 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
+							<h3 className="mt-3 text-xl font-semibold text-emerald-900">
 								{prisdata.title}
 							</h3>
 							<h3 className="mt-3 text-sm text-muted-foreground">
 								{prisdata.description}
 							</h3>
-							<p className="text-base font-medium text-gray-900">
+							<p className="text-base font-semibold text-gray-900">
 								{prisdata.price}
 							</p>
 						</div>
