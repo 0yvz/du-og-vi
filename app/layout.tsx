2a2da9008816cba07439439a75889db4504ffbd1
0yvz <oyvindg@live.no>
2023-12-27 14:16:40 +0100

    Update metadata and layout in RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,31 +10,31 @@ import Footer from "@/components/footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Du & Vi",
-  description: "Hjelp til hjelp",
+	title: "DfOmsorg",
+	description: "Hjelp til hjelp",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" className="!scroll-smooth">
-      <body
-        className={
-          "${inter.className} bg-gray-50 text-gray-950 relative t-24 sm:pt-32"
-        }
-      >
-        <div className=" bg-orange-100 absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-        <div className=" bg-green-200 absolute top-[-1rem] -z-10 left-[11rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
-        <ActiveSectionContextProvider>
-          <Header />
-          {children}
-          <Footer />
-          <Toaster position="bottom-center" />
-        </ActiveSectionContextProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" className="!scroll-smooth">
+			<body
+				className={
+					"${inter.className} bg-gray-50 text-gray-950 relative t-24 sm:pt-32"
+				}
+			>
+				<div className=" bg-orange-100 absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+				<div className=" bg-green-200 absolute top-[-1rem] -z-10 left-[11rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+				<ActiveSectionContextProvider>
+					<Header />
+					{children}
+					<Footer />
+					<Toaster position="bottom-center" />
+				</ActiveSectionContextProvider>
+			</body>
+		</html>
+	);
 }
